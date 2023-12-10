7bf714316b2a1c40a36ef1d6794ed45216cb5c11
MaxThomas90 <56394971+MaxThomas90@users.noreply.github.com>
2023-12-10 14:19:00 +0000

    Initialises RDSDatabaseConnector with credentials dictionary.

--- db_utils.py
+++ db_utils.py
@@ -1,12 +1,13 @@
 import yaml
 
-def load_credentials(filename="credentials.yaml"):
+def load_credentials(filename):
     with open(filename, 'r') as fn:
         credentials_dict = yaml.safe_load(fn)
     return credentials_dict
 
 class RDSDatabaseConnector():
-    pass
+    def __init__(self, filename="credentials.yaml"):
+        self.credentials = load_credentials(filename)
 
 if __name__ == '__main__':
-    print(load_credentials())
+    print(RDSDatabaseConnector())
