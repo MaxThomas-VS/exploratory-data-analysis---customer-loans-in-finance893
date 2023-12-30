411405899a0031724f77dcfa2df2de26b887da02
Max Thomas <maxthomas.vs@gmail.com>
2023-12-30 12:09:12 +0000

    Adds documentation to investigation step.

--- code/investigate_loans.py
+++ code/investigate_loans.py
@@ -18,11 +18,39 @@ import numpy as np
 import seaborn as sns
 
 def get_potential_value(df):
+    '''
+    Calculate the potential value of a loan if it were fully repaid.
+
+    Parameters:
+    ----------
+    df: pandas dataframe
+        some dataframe to calculate the potential value of
+    
+    Returns:
+    --------
+    potential value: float
+        the potential value in GBP
+    '''
     # P: principle [GBP], r: interest rate, t: term [years]
     P, r, t = df['loan_amount'], 1+df['int_rate']/100, df['term_numeric']/12 
     return ( P * ( r ** t ) ).sum()
 
 def get_loss_breakdown(df, loan_status):
+    '''
+    Calculate the key statistics for a given dataset and loan status.
+
+    Parameters:
+    ----------
+    df: pandas dataframe
+        some dataframe 
+    loan_status: str
+        string to match in 'loan_status-simple' column (e.g. 'Current')
+    
+    Returns:
+    --------
+    loss_breakdown: dict
+        the key stats stored in a dictionary
+    '''
     loss_breakdown = {}
     if loan_status:
         df2 = df.loc[df['loan_status-simple']==loan_status]
@@ -40,26 +68,78 @@ def get_loss_breakdown(df, loan_status):
     return loss_breakdown
 
 def label_function(val):
+    ''' Labels for pi chart.'''
     return f'{val / 100 * len(df):.0f}\n{val:.0f}%'
 
 def get_pct_loanstatus(df, status):
+    ''' Get percentage of a given loan status within a dataframe'''
     N = len(df)
     n = df['loan_status-simple'].value_counts()[status]
     return 100 * n / N
     
 def x_pct_of_y(x, y, df):
+    ''' Calculate a percentage.
+
+    Parameters:
+    ----------
+    x, y: str or float
+        either column names for dtatframe (if df provided) 
+        or numbers (if df passed as None)
+    df: pandas dataframe or None
+        either pass a dataframe or None
+    
+    Returns:
+    --------
+    percentage of x wrt y: float
+    '''
     if df is None:
         return 100 * x / y
     else:
         return 100 * df[x].sum() / df[y].sum()
 
 def pct_status(df, column, value, status):
+    ''' Return the percentage of a given value within a column with a given loan status.
+    
+    Parameters:
+    ----------
+    df: pandas dataframe
+    column: str
+        column name within df
+    value: str
+        value within df[column]
+    status: str
+        value within df['loan_status-simple']
+    
+    Returns:
+    --------
+    pct_return: float
+        percentage of given loan status within a subset of the dataframe
+    len_df: int
+        length of subset dataframe
+    '''
     df_subset = df.loc[df[column]==value]
     len_df = len(df_subset)
     num_status = df_subset['loan_status-simple'].value_counts()[status]
-    return 100 * num_status / len_df, len_df
+    pct_return = x_pct_of_y(num_status,len_df,df=None)
+    return pct_return, len_df
 
 def pct_status_series(df, column, status):
+    ''' Loop through categories within a given column and store stats from pct_status.
+
+    Parameters:
+    ----------
+    df: pandas dataframe
+    column: str
+        column name within df
+    status: str
+        value within df['loan_status-simple']
+    
+    Returns:
+    --------
+    pct_stats: pandas dataframe
+        pct_status output for each category stored
+
+    '''
     values = []
     cats = []
     n_in_cat = []
@@ -68,17 +148,30 @@ def pct_status_series(df, column, status):
         n_in_cat.append(num)
         values.append(value)
         cats.append(cg)
-    return pd.DataFrame({'pct': values, 'n':n_in_cat}, index=cats)
-    #return pd.Series(values, index=cats)
+    pct_stats = pd.DataFrame({'pct': values, 'n':n_in_cat}, index=cats)
+    return pct_stats 
 
-def plot_bar(height, labels, Ns=None):
+def plot_bar(height, labels, Ns=None, title=''):
+    ''' Plots a bar chart.
+
+    Parameters:
+    ----------
+    height: list of float
+        height of bars
+    labels: list of str
+        xlabels for bars
+    Ns: list of int or None
+        number of observations for each bar
+    title: str
+        plot title
+    '''
     plt.figure()
     ax = sns.barplot(x=labels, y=height)
     pos = range(len(height))
     if not Ns is None:
         for tick,label in zip(pos,ax.get_xticklabels()):
             ax.text(pos[tick], height[tick], Ns[tick], horizontalalignment='center', rotation=300, color='k', size='small')
-    plt.title(cv)
+    plt.title(title)
     plt.xticks(rotation=45, ha='right')
     plt.xlabel('')
 
@@ -150,8 +243,9 @@ print('%s %% of charged off loans were recovered.' % (insights['recovered_from_c
 print('%s %% of charged off recoveries were recieved after closure of the loan.' % (post_recovery_charged_off))
 
 #plt.bar(x=insights.keys(), height=insights.values())
-plot_bar(height=loss_breakdown['Charged Off'].values(),
-         labels=loss_breakdown['Charged Off'].keys())
+plot_bar(height=[x * 10**-6 for x in list(loss_breakdown['Charged Off'].values())],
+         labels=loss_breakdown['Charged Off'].keys(),
+         title='Financial metrics for charged off loans (million GBP).')
 #plt.bar(x=loss_breakdown['Charged Off'].keys(),
 #        height=loss_breakdown['Charged Off'].values())
 
@@ -184,35 +278,36 @@ print('This is %s %% of the total.' % (100 * loss_breakdown['Late->Charged Off']
 
 
 # %% [markdown]
-# What factors control the likelyhood of charging off loans?
+# What factors control the likelihood of charging off loans?
 # First, we visualise the impact of the categorical variables.
 # The height of the bars represents the percentage of loans in a given category that were charged off, while the overlying number gives the total customers in that category.
 # Taking *term*, below, as an example, we see that around 9 % of 36 month loans were charged off whereas around 15 % of 60 month loans were charged off. The sample size is large for both categories (well over 10000 in each), so we expect these percentages to be robust.
 #
-# *grade* is a good predictor of the likelyhood of charge off. Each decrease in *grade* makes charge off more likely. From the best (A) to the worst (G) gradings, we find a 23.6 % increase in the likelyhood of charge off. 
+# *grade* is a good predictor of the likelihood of charge off. Each decrease in *grade* makes charge off more likely. From the best (A) to the worst (G) gradings, we find a 23.6 % increase in the likelihood of charge off. 
 #
-# *sub_grade* is also clearly related to the likelyhood of charge off, though smaller sample sizes introduce extra variability relative to *grade*.
+# *sub_grade* is also clearly related to the likelihood of charge off, though smaller sample sizes introduce extra variability relative to *grade*.
 #
-# *employment_length* shows no clear trend with likelyhood of charge off.
+# *employment_length* shows no clear trend with likelihood of charge off.
 #
 # Renters are more likely that mortgage payers to charge off by 2.3 %. Outright owners have a similar charge off probability to renters, which is a surprising result. Further investigation into the causes of relatively high rates of charge off in outright home owners is warranted.
 #
 # Small buisness related loans stand out as having especially high charge off rates. Over half of the loans were for debt consolidation, and the charge off rates for these are 0.7 % lower than the charg off rates for the entire loan portfolio.
 # %%
 data_frame_info = dbu.DataFrameInfo()
-iscat = data_frame_info.IsCategorical(df)
-
+#iscat = data_frame_info.IsCategorical(df)
+iscat = ['term','grade','sub_grade','employment_length','home_ownership','purpose']
 cat_pct = {}
 for cv in iscat:
     cat_pct[cv] = pct_status_series(df, cv, 'Charged Off')
     plot_bar(height=cat_pct[cv]['pct'].values,
              labels=cat_pct[cv].index,
-             Ns=cat_pct[cv]['n'].values)
+             Ns=cat_pct[cv]['n'].values,
+             title='% of loans charged off, ' + cv)
 
 # %% [markdown]
 # We investigate the numerical variables by visualising their probability density function (PDF). The horizontal axis gives the range of values for a given variable, while the vertical axis gives the probability of that value appearing in the dataset. 
 # We calculate and plot PDF for subsets of the dataset (*Fully Paid*, *Current*, *Charged Off*, and *Late*).
-# Differences between *Fully Paid* and *Charged Off* indicate that a given variable could be useful in predicting the likelyhood of charge off.
+# Differences between *Fully Paid* and *Charged Off* indicate that a given variable could be useful in predicting the likelihood of charge off.
 # We can also compare the *Late* and *Current* categories to the *Fully Paid* and *Charged Off* categories. For example, If the PDF for *Current* is more similar to *Fully Paid* than *Charged Off* it suggests less risk in the current loans.
 #
 # *dti* is a measure of the debt burden of the loan relative to the customers total debt. Lower values indicate that repayments on the loan are less burdonsome. For *Fully Paid* loans, *dti* is indeed generally lower than for *Charged Off*. For *Current* and *Late* loans, *dti* is more similar to *Charged Off* than *Fully Paid*.
@@ -221,7 +316,7 @@ for cv in iscat:
 #
 # The interest rate is higher in *Charged Off* (median=14.35 %) than in *Fully Paid* (median=12.99 %). Interest rates for *Late* customers are more similar to *Charged Off*, while *Current* customers have rates more similar to *Fully Paid*.
 #%%
-for variable in ['loan_amount','dti','int_rate','instalment','annual_inc']:
+for variable in ['dti','int_rate','instalment','annual_inc']:
     fig, ax = plt.subplots(1,1)
     colours = ['black','green','red','blue']
     statuss = ['Fully Paid', 'Current', 'Charged Off', 'Late']
@@ -232,5 +327,6 @@ for variable in ['loan_amount','dti','int_rate','instalment','annual_inc']:
         sns.kdeplot(to_plot, label=label, color=colour)
         plt.axvline(median, color=colour, linestyle='dashed', linewidth=1)
     plt.legend(loc='upper right')
+    plt.title('Probability desnsity function for ' + variable)
         #sns.histplot(to_plot, kde=True, element='step', fill=False, stat='density', ax=ax, hue='white')
 # %%
